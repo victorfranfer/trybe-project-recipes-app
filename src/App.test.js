56887b68c63a32197b95b8cd0f4ae5c70d94591a
Lucas Ferreira <lucasfpnt@gmail.com>
2022-07-14 20:31:39 -0300

    requisito 5 e 6 - Login concluído

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,4 @@ import App from './App';
 
 test('Farewell, front-end', () => {
   render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
 });
