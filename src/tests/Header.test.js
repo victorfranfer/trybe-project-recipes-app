57e3f7995fc4fb4e320310424fab882c9bc185b7
Rafael Martins <rmartins858@gmail.com>
2022-07-18 14:11:08 -0300

    Cobertura dos testes header

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderWithRouter from './renderWithRouter';
-import { cleanup, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Foods from '../pages/Foods';
 import Drinks from '../pages/Drinks';
 import Profile from '../pages/Profile';
@@ -10,6 +11,7 @@ import DoneRecipes from '../pages/DoneRecipes';
 import DrinksInProgress from '../pages/DrinksInProgress';
 import FoodsInProgress from '../pages/FoodsInProgress';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
+import App from '../App'
 
 describe('Testando os componentes do header', () => {
   test('testando componetes da pagina Foods', () => {
@@ -80,4 +82,25 @@ describe('Testando os componentes do header', () => {
     });
     expect(titlePage).toBeInTheDocument();
   });
+
+  test('testando o input search', () => {
+    renderWithRouter(<App />);
+    const input = screen.getByTestId("email-input");
+    const password = screen.getByTestId("password-input");
+    const submitBtn = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(input, "lucasfpnt@gmail.com");
+    userEvent.type(password, "1234567");
+    userEvent.click(submitBtn)
+
+    const searchField = screen.getByTestId('search-top-btn')
+    userEvent.click(searchField)
+
+    const searchBar = screen.getByTestId('search-input')
+    expect(searchBar).toBeInTheDocument()
+    userEvent.click(searchField)
+    expect(searchBar).not.toBeInTheDocument()
+  });
+
+  
 });
